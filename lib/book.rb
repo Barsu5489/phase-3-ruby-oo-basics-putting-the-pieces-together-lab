2551a27b53e5041c19c93178e36dcd9b21271778
barsu5489 <ebarsulai@gmail.com>
2023-02-10 02:38:17 +0300

    books

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
+class Book
+    
+    attr_accessor :title, :author, :genre, :page_count
+ def initialize (title= 'And Then There Were None',author)
+    @title = title
+    @author = author
+ end
+ def turn_page
+    puts "Flipping the page...wow, you read fast!"
+ end
 
+end
 
